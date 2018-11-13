01ff32f862597c44cef3fedd552a4eaccedb91fd
Yoshihisa Murakami <muranet+git@gmail.com>
2018-11-13 09:07:15 +0000

    script.js作成

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 function search()
 {
-    alert('ok');
+  var word = document.getElementById('word').value;
+  var word_enc = encodeURIComponent(word);
+  var url = "https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=" + word_enc;
+  window.open(url, '_blank');
 }
 
 /*
@@ -23,16 +26,39 @@ https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%
 URLを短くしてみる(3) ieパラメータを削ってみた OK
 https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
 
-URLを短くしてみる(4) 検索結果が変わってしまった NG
+URLを短くしてみる(4) rhパラメータを削ってみた -> NG (検索結果が変わってしまった)
 https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
 
-URLを短くしてみる(5) (rubyでURLデコード+エンコード) NG kindle unlimited検索ではなくなってしまった
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2C&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+# rhパラメータをURLデコードすると
+# rh=n:2250738051,k:PHPサイエンス,p_n_feature_nineteen_browse-bin:3169286051
 
-URLを短くしてみる(6) (rubyでURLデコード+エンコード)  OK! 検索結果51件
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=n:2250738051,p_n_feature_nineteen_browse-bin:3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+# このうち、
+# rh=n:2250738051,k:PHPサイエンス
+# をURLエンコードすると
+# rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
 
-URLを短くしてみる(7) (rubyでURLデコード+エンコード) NG  表示が１列から４列に変わった
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=p_n_feature_nineteen_browse-bin:3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+
+URLを短くしてみる(5) (rhのうちp_n_feature_nineteen_browse-bin) NG 「読み放題」対象の検索ではなくなってしまった
+https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+
+# rh=n:2250738051,p_n_feature_nineteen_browse-bin:3169286051
+# をURLエンコードすると
+# rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051
+
+URLを短くしてみる(6) (rhのうちk)  OK! 検索結果51件
+https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+
+# rh=p_n_feature_nineteen_browse-bin:3169286051
+# をURLエンコードすると
+# rh=p_n_feature_nineteen_browse-bin%3A3169286051
+
+URLを短くしてみる(7) (rhのうちn) NG 検索結果が変わってしまった
+https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=p_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+
+URLを短くしてみる(8) fstパラメータを削った OK
+https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+
+URLを短くしてみる(9) s/ を１つ削った  OK
+https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
 
 */
--- t01.rb
+++ t01.rb
@@ -1,16 +1,15 @@
 require 'uri'
-# puts URI.unescape('https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9')
+require 'cgi'
+
+#puts URI.unescape('rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051')
 
 # 結果
-# https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=n:2250738051,k:PHPサイエンス,p_n_feature_nineteen_browse-bin:3169286051&keywords=PHPサイエンス
+# rh=n:2250738051,k:PHPサイエンス,p_n_feature_nineteen_browse-bin:3169286051
 
-# p_n_feature_nineteen_browse-bin:3169286051 は kindle unlimited検索を指定
+#(a)rh=n:2250738051,k:PHPサイエンス
+#puts CGI.escape('n:2250738051,k:PHPサイエンス')
 
-# https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=n:2250738051,p_n_feature_nineteen_browse-bin:3169286051&keywords=PHPサイエンス
-# (6)
-# p URI.escape('https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=n:2250738051,p_n_feature_nineteen_browse-bin:3169286051&keywords=PHPサイエンス')
+#(b)n:2250738051,p_n_feature_nineteen_browse-bin:3169286051
+#puts CGI.escape('n:2250738051,p_n_feature_nineteen_browse-bin:3169286051')
 
-# https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=n:2250738051,p_n_feature_nineteen_browse-bin:3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-# (7)
-p URI.escape('https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=p_n_feature_nineteen_browse-bin:3169286051&keywords=PHPサイエンス')
-#=> https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as:off&rh=p_n_feature_nineteen_browse-bin:3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+puts CGI.escape('p_n_feature_nineteen_browse-bin:3169286051')
