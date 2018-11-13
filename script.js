c6e56604250da00e9010b1953da722fee564339c
Yoshihisa Murakami <muranet+git@gmail.com>
2018-11-14 06:57:08 +0900

    script.jsのメモを整理

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+/**
+ * kindle unlimited検索
+ *
+ * 2018-11-14版
+ */
+
+ /*
+ - amazon検索ボックスで"kindkeストア"を選択、キーワード"PHPサイエンス" で検索し、左枠"読み放題対象タイトル"にチェック
+ https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&ie=UTF8&qid=1541941124&rnid=3169285051
+
+ - 上記URLからパラメータrnidを削る
+ https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&ie=UTF8&qid=1541941124
+ -> OK
+
+ - 同様、qidパラメータを削る
+ https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&ie=UTF8
+ -> OK
+
+ - ieパラメータを削る
+ https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+ -> OK
+
+ - rhパラメータを削る
+ https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+ -> NG （検索結果が変わってしまった）
+
+ - rhパラメータの値を削る(1)
+ rhパラメータをURLデコードすると
+   n:2250738051,k:PHPサイエンス,p_n_feature_nineteen_browse-bin:3169286051
+
+ となる。このうち "p_n_feature_nineteen_browse-bin:3169286051" を削って
+   n:2250738051,k:PHPサイエンス
+ これをURLエンコードすると
+   n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+ これでURLを置き換えて
+   https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+ -> NG (読み放題対象タイトルのチェックが外れてしまった)
+
+- rhパラメータの値を削る(2)
+  n:2250738051,k:PHPサイエンス,p_n_feature_nineteen_browse-bin:3169286051
+  ↓ k:〜を削る
+  n:2250738051,p_n_feature_nineteen_browse-bin:3169286051
+  ↓ URLエンコード
+  n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051
+  これでURLを置き換えると
+  https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+  -> OK
+
+- rhパラメータの値を削る(3)
+  n:2250738051,p_n_feature_nineteen_browse-bin:3169286051
+  ↓  n:〜を削る
+  p_n_feature_nineteen_browse-bin:3169286051
+  ↓ URLエンコード
+  p_n_feature_nineteen_browse-bin%3A3169286051
+  これでURLを置き換えると
+  https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=p_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+  -> NG (kindleストアでの検索ではなくなってしまった)
+
+- fstパラメータを削る
+  https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+  -> OK
+
+- URLの s/ を削る
+  https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
+  -> OK
+
+ */
 
 function search()
 {
@@ -6,59 +73,3 @@ function search()
   var url = "https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=" + word_enc;
   window.open(url, '_blank');
 }
-
-/*
-kindleストア、"abcde" で検索した場合
-https://www.amazon.co.jp/s/ref=nb_sb_noss_2?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Ddigital-text&field-keywords=abcde
-
-”PHPサイエンス”で検索した場合
-https://www.amazon.co.jp/s/ref=nb_sb_noss_2?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Ddigital-text&field-keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-"PHPサイエンス" で検索し、"読み放題対象タイトル"にチェック
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&ie=UTF8&qid=1541941124&rnid=3169285051
-
-URLを短くしてみる(1) rnidパラメータを削ってみた OK
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&ie=UTF8&qid=1541941124
-
-URLを短くしてみる(2) qidパラメータを削ってみた OK
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&ie=UTF8
-
-URLを短くしてみる(3) ieパラメータを削ってみた OK
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-URLを短くしてみる(4) rhパラメータを削ってみた -> NG (検索結果が変わってしまった)
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-# rhパラメータをURLデコードすると
-# rh=n:2250738051,k:PHPサイエンス,p_n_feature_nineteen_browse-bin:3169286051
-
-# このうち、
-# rh=n:2250738051,k:PHPサイエンス
-# をURLエンコードすると
-# rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-
-URLを短くしてみる(5) (rhのうちp_n_feature_nineteen_browse-bin) NG 「読み放題」対象の検索ではなくなってしまった
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Ck%3APHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-# rh=n:2250738051,p_n_feature_nineteen_browse-bin:3169286051
-# をURLエンコードすると
-# rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051
-
-URLを短くしてみる(6) (rhのうちk)  OK! 検索結果51件
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-# rh=p_n_feature_nineteen_browse-bin:3169286051
-# をURLエンコードすると
-# rh=p_n_feature_nineteen_browse-bin%3A3169286051
-
-URLを短くしてみる(7) (rhのうちn) NG 検索結果が変わってしまった
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?fst=as%3Aoff&rh=p_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-URLを短くしてみる(8) fstパラメータを削った OK
-https://www.amazon.co.jp/s/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-URLを短くしてみる(9) s/ を１つ削った  OK
-https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&keywords=PHP%E3%82%B5%E3%82%A4%E3%82%A8%E3%83%B3%E3%82%B9
-
-*/
