5adbdebe04bfdb42b523d10c05f5d6293fd67ac9
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-07-16 18:27:25 +0900

    ページ訪問時に Input にフォーカスを当てる

--- script.js
+++ script.js
@@ -10,3 +10,5 @@ function search()
   var url = 'https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n%3A2250738051%2Ck%3A' + word_enc + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051'
   window.open(url, '_blank');
 }
+
+document.getElementById('word').focus();
